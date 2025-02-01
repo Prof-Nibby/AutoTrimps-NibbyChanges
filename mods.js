09e03bc2f35764165d24868338517c87e42c6b52
Prof-Nibby <professornibbles@live.com>
2025-02-01 15:44:33 -0600

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Zek';
-script.src = 'https://github.com/Prof-Nibby/AutoTrimps-NibbyChanges';
+script.src = 'https://github.com/Prof-Nibby/AutoTrimps-NibbyChanges/AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
 
