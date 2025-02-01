f7ff77b7fed7f716d7acf7038d30331a4fa4e084
Prof-Nibby <professornibbles@live.com>
2025-02-01 16:16:04 -0600

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -29,7 +29,7 @@ function initializeAutoTrimps() {
     for (var m in ATmoduleList) {
         ATscriptLoad(modulepath, ATmoduleList[m]);
     }
-    debug('AutoTrimps - Zek Fork Loaded! - Nibby Changes', '*spinner3');
+    debug('AutoTrimps - Zek Fork Loaded! Nibby Changes', '*spinner3');
 }
 
 var changelogList = [];
