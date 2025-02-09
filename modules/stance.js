27da295b51539a5086937c6b96967fa8eb9ef9f9
Prof-Nibby <professornibbles@live.com>
2025-02-08 21:14:33 -0600

    Update stance.js

--- modules/stance.js
+++ modules/stance.js
@@ -286,7 +286,7 @@ function autoStance2() {
       if (!game.upgrades.Formations.done) return;
       if (game.global.world <= 70) return;
            if (game.global.formation != 2)
-               setFormation(1);
+               setFormation(2);
 }
 
 function windStance() {
