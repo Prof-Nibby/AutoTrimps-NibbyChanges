51f31666bd153f8fbe32b3ec16f3d2c1799d2b25
Prof-Nibby <professornibbles@live.com>
2025-03-28 11:15:28 -0500

    Update other.js

--- modules/other.js
+++ modules/other.js
@@ -2007,7 +2007,7 @@ function ATspirebreed() {
 }
 
 function fightalways() {
-    if (game.global.gridArray.length === 0 || game.global.preMapsActive || !game.upgrades.Battle.done || game.global.fighting || (game.global.spireActive && game.global.world >= getPageSetting('IgnoreSpiresUntil')))
+    if (game.global.gridArray.length === 0 || game.global.preMapsActive || !game.upgrades.Battle.done || game.global.fighting)
         return;
     if (!game.global.fighting)
         fightManual();
